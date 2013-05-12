d809b7d68f3582fb5c417749df5af6b4bf681c18
Raimondas aka MekDrop <github@mekdrop.name>
2013-05-12 16:06:21 +0300

    untop changed value

--- action/untop.php
+++ action/untop.php
@@ -12,7 +12,8 @@ class actionUnTop implements iAction {
     
     public function exec(array $params) {
         $db = gcDB::getInstance();
-        $db->query('UPDATE top SET score = score - 1000');
+        $db->query('UPDATE top SET score = score - 7000');
+        $db->query('DELETE FROM top WHERE score < -10000');
     }   
     
 }
